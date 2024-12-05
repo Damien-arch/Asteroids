815476301fb9c60ae8ad4939a68f5e9adbfe12ff
Gage-Atlas <Gagebreedon@gmail.com>
2024-12-05 02:32:29 -0500

    Movement update

--- Assets/Scripts/TestMovement.cs
+++ Assets/Scripts/TestMovement.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class TestMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
+        // Move Up with 'W'
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * Time.deltaTime * 10.0f;
+        }
+
+        // Move Left with 'A'
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * 10.0f;
+        }
+
+        // Move Down with 'S'
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * 10.0f;
+        }
+
+        // Move Right with 'D'
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * 10.0f;
         }
     }
 }
