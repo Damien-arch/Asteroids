d0f0642cf5b30f1d70a2bd7133c7748058f1d7d2
Damien-arch <200602230@student.georgianc.on.ca>
2024-12-08 22:13:09 -0500

    Reapply "Game over screen"
    This reverts commit 524f51cf4d59e57e65a77f0142bcdd173f4cd28c.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,36 +1,38 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class PlayerCollision : MonoBehaviour
 {
     private bool isDestroyed = false;
+
+    public GameObject gameOverUI;
     void Start()
     {
 
     }
-    
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Asteroid") && !isDestroyed)
         {
-            isDestroyed = true;
-            Debug.Log("Player hit! restarting...");
-
-            foreach (var renderer in GetComponentsInChildren<Renderer>())
-            {
-                renderer.enabled = false; 
-            }
-            foreach (var collider in GetComponents<Collider2D>())
-            {
-                collider.enabled = false;
-            }
-        }
+           foreach (var renderer in GetComponentsInChildren<Renderer>())
+           {
+              renderer.enabled = false;
+           }
+           foreach (var collider in GetComponents<Collider2D>())
+           {
+              collider.enabled = false;
+           }
+        }   
     }
-    IEnumerator RestartGameAfterSound(float delay)
+    IEnumerator ShowGameOverMenu(float delay)
     {
         yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        if (gameOverUI != null)
+        {
+            gameOverUI.SetActive(true);
+        }
     }
 }
